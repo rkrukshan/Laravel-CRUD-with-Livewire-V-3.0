310c37b40ea221c4e6698794df267b022c5eb594
RK Rukshan <44175424+RkRukshan@users.noreply.github.com>
2023-11-10 23:52:46 +0530

    modify contents of create.blade.php

--- .history/resources/views/livewire/create.blade_20231110235139.php
+++ .history/resources/views/livewire/create.blade_20231110235139.php
@@ -4,14 +4,12 @@
     <div class="mb-3">
       <label for="title" class="form-label">Title</label>
       <input type="text" class="form-control" id="title">
+    </div>
 
-
+    <div class="mb-3 form-check">
       <label for="body" class="form-label">Body</label>
         <textarea type="email" name="body" id="body" class="form-control" cols="30" rows="10"></textarea>
-   
-
     </div>
-    <center>
+    <center></center>
     <button type="submit" class="btn btn-success">Save</button>
-</center>
   </form>
--- .history/resources/views/livewire/create.blade_20231110235221.php
+++ .history/resources/views/livewire/create.blade_20231110235221.php
@@ -4,12 +4,10 @@
     <div class="mb-3">
       <label for="title" class="form-label">Title</label>
       <input type="text" class="form-control" id="title">
+    </div>
 
-
-      <label for="body" class="form-label">Body</label>
-        <textarea type="email" name="body" id="body" class="form-control" cols="30" rows="10"></textarea>
-   
-
+    <div class="mb-3 form-check">
+      
     </div>
     <center>
     <button type="submit" class="btn btn-success">Save</button>
